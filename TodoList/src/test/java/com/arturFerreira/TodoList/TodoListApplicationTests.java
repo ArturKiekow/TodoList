05fc1354af21da23f1afee9c2556f756974bb710
ArturKiekow <arturkiekow@gmail.com>
2024-12-16 09:16:00 -0300

    adicionando estrutura inicial do projeto

---
+++ TodoList/src/test/java/com/arturFerreira/TodoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturFerreira.TodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
